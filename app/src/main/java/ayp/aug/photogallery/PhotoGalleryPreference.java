86421e51bd021db5746b91ff57b38eb2ee835114
Palm <waraporn.v@augmentis.biz>
2016-08-22 15:57:49 +0700

    edit pollService

--- app/src/main/java/ayp/aug/photogallery/PhotoGalleryPreference.java
+++ app/src/main/java/ayp/aug/photogallery/PhotoGalleryPreference.java
@@ -11,6 +11,7 @@ public class PhotoGalleryPreference {
 
     private static final String TAG = "PhotoGalleryPref";
     private static final String PREF_SEARCH_KEY = "PhotoGalleryPref";
+    private static final String PREF_LAST_ID = "PREF_LAST_ID";
 
     public static String getStoredSearchKey(Context context) {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
@@ -21,6 +22,18 @@ public class PhotoGalleryPreference {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
         pref.edit()
                 .putString(PREF_SEARCH_KEY, key)
+                .apply();
+    }
+
+    public static String getStoredLastId(Context context) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        return pref.getString(PREF_LAST_ID, null);
+    }
+
+    public static void setStoredLastId(Context context, String key) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        pref.edit()
+                .putString(PREF_LAST_ID, key)
                 .apply();
     }
 }
--- app/src/main/java/ayp/aug/photogallery/PollService.java
+++ app/src/main/java/ayp/aug/photogallery/PollService.java
@@ -1,10 +1,19 @@
 package ayp.aug.photogallery;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Waraporn on 8/22/2016.
@@ -13,10 +22,29 @@ public class PollService extends IntentService {
 
     private static final String TAG = "PollService";
 
+    private static final int POLL_INTERVAL = 1000*60; //60 sec
+
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
 
+    public static void setServiceAlarm(Context c, boolean isOn){
+        Intent i = PollService.newIntent(c);
+        PendingIntent pi = PendingIntent.getService(c, 0, i, 0);
+
+        AlarmManager am = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+
+        if (isOn) {
+            //AlarmManager.RTC -> System.currentTimeMillis();
+            am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,   //param1: Mode
+                    SystemClock.elapsedRealtime(),                  //param2: Start
+                    POLL_INTERVAL,                                  //param3: Interval
+                    pi);                                            //param4: Pending action(intent)
+        } else {
+            am.cancel(pi); //cancel interval call
+            pi.cancel(); // cancel pending intent call
+        }
+    }
     public PollService() {
         super(TAG);
     }
@@ -24,5 +52,43 @@ public class PollService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.i(TAG, "Recieve a call from intent: " + intent);
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+        Log.i(TAG, "Active network!");
+
+        String query = PhotoGalleryPreference.getStoredSearchKey(this);
+        String storedId = PhotoGalleryPreference.getStoredLastId(this);
+
+        List<GalleryItem> galleryItemList = new ArrayList<>();
+
+        FlickrFetcher flickrFetcher = new FlickrFetcher();
+        if(query == null) {
+            flickrFetcher.getRecentPhotos(galleryItemList);
+        } else {
+            flickrFetcher.searchPhotos(galleryItemList, query);
+        }
+
+        if (galleryItemList.size() == 0) {
+            return;
+        }
+
+        Log.i(TAG, "Found search or te ited");
+
+        String newestId = galleryItemList.get(0).getId(); // fetching first item
+
+        if (newestId.equals(storedId)) {
+            Log.i(TAG, "No new item");
+        } else {
+            Log.i(TAG, "New item found");
+        }
+        PhotoGalleryPreference.setStoredLastId(this, newestId);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        boolean isActiveNetwork = cm.getActiveNetworkInfo() != null;
+        boolean isActiveNetworkConnected = isActiveNetwork && cm.getActiveNetworkInfo().isConnected();
+        return isActiveNetworkConnected;
     }
 }
